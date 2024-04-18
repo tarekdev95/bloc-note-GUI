a551dc6f5117d96e2e0835c7cfc6031403a1a78a
ATEL2005 <166388061+ATEL2005@users.noreply.github.com>
2024-04-18 12:13:31 +0200

    Update bloc.py

--- bloc.py
+++ bloc.py
@@ -4,6 +4,11 @@ from tkinter import dialog
 root = tk.Tk()
 root.title('Mini-bloc')
 root.geometry("300x400")
+#creation de la menu bar
+menu_bar = tk.Menu(root)
+#ajouter un menu deroulant appelle fichier au menu bar
+Fichier_menu = tk.Menu(menu_bar , tearoff=0) # la fonctionne tearoff empeche le detachment du menu de la menu bar
+menu_bar.add_cascade(label="Fichier" , menu=Fichier_menu , activebackground="black" , activeforeground="white" , background="white" , foreground="black")
 
-
+root.config(menu=menu_bar)
 root.mainloop()
