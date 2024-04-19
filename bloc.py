e96737e59b9b930f2a4b2f210ed59e34bb64c0d9
tarekdev95 <tarek.bordjah@gmail.com>
2024-04-19 03:01:09 +0200

    barre de menu et la fonction d'ouverture des fichiers

--- bloc.py
+++ bloc.py
@@ -1,11 +1,52 @@
 import tkinter as tk
-from tkinter import dialog
+from tkinter import filedialog
 
+# ouvrir un fichier 
+def ouvrir():
+    text_area.delete(tk.END)
+    ouvrir_fichier = filedialog.askopenfilename(title='selectionner un fichier',defaultextension='.txt', filetypes=[('fichier', '*.txt')])
+    if ouvrir_fichier is not None:  
+        file =  open(ouvrir_fichier, 'r', encoding='utf-8')
+        lecture = file.readlines()
+        for ligne in lecture:
+            text_area.insert(tk.END, ligne)
+# declarer la fenetre principale
 root = tk.Tk()
 root.title('Mini-bloc')
 root.geometry("300x400")
 
+# espace de saisi
+text_area = tk.Text(root)
+text_area.pack(fill="both", expand=True)
 
-btn = tk.Button(root, text='cliquer')
+# menu principale
+main_menu = tk.Menu(root)
 
+# sous menu 1 (fichier)
+fichier = tk.Menu(main_menu, tearoff=0)
+fichier.add_command(label="ouvrir un fichier", command=ouvrir)
+fichier.add_command(label="Enregistrer")
+fichier.add_command(label="Enregistrer sous")
+fichier.add_command(label="Quitter", command=root.quit)
+main_menu.add_cascade(label="fichier", menu=fichier)
+
+# sous menu 2 (editions)
+edition = tk.Menu(main_menu, tearoff=0)
+edition.add_command(label="copier-coller")
+main_menu.add_cascade(label="edition", menu=edition)
+
+# Sous menu 3 (Option)
+option = tk.Menu(main_menu, tearoff=0)
+option.add_command(label="Taille de texte")
+option.add_command(label="Couleur")
+main_menu.add_cascade(label="Option", menu=option)
+
+# sous menu 4 (outil)
+outil = tk.Menu(main_menu, tearoff=0)
+outil.add_command(label="Calculatrice")
+main_menu.add_cascade(label="Outil", menu=outil)
+
+
+
+root.config(menu=main_menu)
 root.mainloop()
