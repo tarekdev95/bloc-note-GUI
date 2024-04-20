441882f9627ffe0dbbecb311e9a768c6556cd9f9
ATEL2005 <awatsoptchoupouevaleslie@gmail.com>
2024-04-20 19:37:42 +0200

    second commit

--- bloc.py
+++ bloc.py
@@ -1,5 +1,10 @@
 import tkinter as tk
-from tkinter import filedialog
+from tkinter import filedialog 
+from tkinter import messagebox
+
+#creer un nouveau fichier
+def nouveau():
+    texte.delete('1.0' , tk.END)
 
 # ouvrir un fichier 
 def ouvrir():
@@ -10,20 +15,23 @@ def ouvrir():
         lecture = file.readlines()
         for ligne in lecture:
             text_area.insert(tk.END, ligne)
-#enregistre un fichier
-def enregistrer():
-    with open(filename ,'w') as file:
-        contenu = texte.get("1.0" , "end-1c")
-        file.write(contenu)
 
-#enregistre un fichier en fichier.txt
+# enregistre un fichier
+def enregistre():
+    file_name = filedialog.askopenfilename(mode = 'w' , dafaultextension = ".txt")
+    if file_name:
+        with open(file_name , 'r') as file:
+            file_content = file.read()
+            text_area.delete('1.0' , tk.END)
+            text_area.insert(tk.END , file_content)
+
+# enregistre un fichier en fichier.txt
 def enregistrer_sous():
-    f = filedialog.asksaveasfile(mode='w' , defaultextension=".txt")
-    if f is None:
-        return
-    contenu = texte.get("1.0" , "end-1c")
-    f.write(contenu)
-    f.close()
+    fichier = filedialog.asksaveasfile(mode='w' , defaultextension=".txt")
+    if fichier is None:
+        contenu = text.get("1.0" , tk.END)
+        with open(fichier , 'r') as file:
+            file.read(contenu)
 # declarer la fenetre principale
 root = tk.Tk()
 root.title('Mini-bloc')
@@ -38,15 +46,17 @@ main_menu = tk.Menu(root)
 
 # sous menu 1 (fichier)
 fichier = tk.Menu(main_menu, tearoff=0)
+fichier.add_command(label="nouveau fichier" , command=nouveau)
 fichier.add_command(label="ouvrir un fichier", command=ouvrir)
-fichier.add_command(label="Enregistrer" , command=enregistrer)
+fichier.add_command(label="Enregistrer" , command=enregistre)
 fichier.add_command(label="Enregistrer sous" , command=enregistrer_sous)
 fichier.add_command(label="Quitter", command=root.quit)
 main_menu.add_cascade(label="fichier", menu=fichier)
 
 # sous menu 2 (editions)
 edition = tk.Menu(main_menu, tearoff=0)
-edition.add_command(label="copier-coller")
+edition.add_command(label="copier")
+edition.add_command(label="coller")
 main_menu.add_cascade(label="edition", menu=edition)
 
 # Sous menu 3 (Option)
@@ -61,12 +71,9 @@ outil.add_command(label="Calculatrice")
 main_menu.add_cascade(label="Outil", menu=outil)
 
 
-#zone de texte
-texte = tk.Text()
-texte.pack(expand=True , fill='both')
+#nom du fichier pas defaut
+file_name = "notes.txt"
 
-#nom pas defaut du fichier
-filename = "notes.txt"
 
 root.config(menu=main_menu)
 root.mainloop()
